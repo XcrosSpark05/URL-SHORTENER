e3fc399eac0fb3807f829abed71820d45168b9e5
Prathamesh Karbele <karbeleprathamesh@gamil.com>
2025-11-27 21:10:37 +0530

    CreateUrl function block done with all the try catch handled

--- server-app/src/controllers/shortUrl.ts
+++ server-app/src/controllers/shortUrl.ts
@@ -5,7 +5,22 @@ import { urlModel } from "../model/shortUrl";
 export const createUrl = async (
     req: express.Request,
     res: express.Response
-) => {};
+) => {
+    try {
+        console.log("The fullUrl is ", req.body.fullUrl);
+        const {fullUrl} =  req.body;
+        const urlFound = await urlModel.find({fullUrl});
+        if(urlFound.length > 0){
+            res.status(409);
+            res.send(urlFound);
+        }else{
+            const shortUrl = await urlModel.create({fullUrl});
+            res.status(201).send(shortUrl);
+        }
+    } catch (error) {
+        res.status(500).send({ message: "Something went Wrong!!"});
+    }
+};
 
 
 
